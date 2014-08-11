abdd96809c03c855c5b307a7d07742b999ba1544
Joe Dowdell <joedowdell@gmail.com>
2014-08-11 19:07:06 +0100

    creating new restaurant feature

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,32 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def new
+  	@restaurant = Restaurant.new
+  end
+
+  def create
+  	Restaurant.create(params[:restaurant].permit(:name))
+  	redirect_to '/restaurants'
+  end
+
+  def edit
+  	@restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+  	@restaurant = Restaurant.find(params[:id])
+  	@restaurant.update(params[:restaurant].permit(:name))
+  
+  	redirect_to '/restaurants'
+  end
+
+  def destory
+  	@restaurant = Restaurant.find(params[:id])
+  	@restaurant.destroy
+
+  	flash[:notice] = 'Restaurant deleted successfully'
+  	redirect_to '/restaurants'
+  end
+
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -14,10 +14,62 @@ describe 'restaurants' do
 		before(:each) do
 			Restaurant.create(name: 'Nandos') 
 		end
+		
 		it 'should display them' do
 			visit '/restaurants'
 			expect(page).to have_content 'Nandos'
 		end
 	end
 
+	context 'creating restaurants' do
+		
+		it 'prompts the user to fill out a form, then display new restaurant' do
+			visit '/restaurants'
+			click_link 'Add a restaurant'
+
+			fill_in 'Name', with: 'Chipotle'
+			click_button 'Create Restaurant'
+
+			expect(page).to have_content 'Chipotle'
+			expect(current_path).to eq '/restaurants'
+		end
+
+	end
+
+	context 'edit restaurants' do
+		before(:each) do
+			Restaurant.create(name: 'Chilpotle') 
+		end
+
+		it 'prompts the user to fill out a form, then display edited restaurant' do
+			visit '/restaurants'
+			click_link 'Edit Chipotle'
+
+			fill_in 'Name', with: 'Chipotle'
+			click_button 'Update Restaurant'
+
+			expect(page).to have_content 'Chipotle'
+			expect(current_path).to eq '/restaurants'
+
+		end
+
+	end
+
+	context 'delete restaurant' do
+
+		it 'removes restaurant when a user clicks a delete link' do
+			visit '/restaurants'
+			click_link 'Delete Chipotle'
+
+			expect(page).not_to have_content 'Chipotle'
+			expect(page).to have_content 'Restaurant deleted successfully'
+		end
+
+	end
+
+
+
+
 end
+
+
