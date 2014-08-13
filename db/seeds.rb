276e6b2d02f1c60b1edf5d6c0f7c7edacd1e6d55
Joe Dowdell <joedowdell@gmail.com>
2014-08-13 15:53:11 +0100

    Devise Gemfile added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+kfc = Restaurant.create(name: 'KFC')
+kfc.reviews.create(rating: 5, thoughts: 'Great')
+kfc.reviews.create(rating: 4, thoughts: 'Good')
+kfc.reviews.create(rating: 3, thoughts: 'OK')
+kfc.reviews.create(rating: 2, thoughts: 'Poor')
+kfc.reviews.create(rating: 1, thoughts: 'Bad')
+
+nandos = Restaurant.create(name: 'Nandos')
+nandos.reviews.create(rating: 5, thoughts: 'Really Great')
+nandos.reviews.create(rating: 4, thoughts: 'Really Good')
+nandos.reviews.create(rating: 3, thoughts: 'Ready OK')
+nandos.reviews.create(rating: 2, thoughts: 'Ready Poor')
+nandos.reviews.create(rating: 1, thoughts: 'Really Bad')
+
+
+chipotle = Restaurant.create(name: 'Chipotle')
+chipotle.reviews.create(rating: 5, thoughts: 'Really Great')
+chipotle.reviews.create(rating: 4, thoughts: 'Really Good')
+chipotle.reviews.create(rating: 3, thoughts: 'Ready OK')
+chipotle.reviews.create(rating: 2, thoughts: 'Ready Poor')
+chipotle.reviews.create(rating: 1, thoughts: 'Really Bad')
+
+
---
+++ spec/features/endorsements_feature_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'endorsemnts reviews' do
+	before do
+		kfc = Restaurant.create(name: 'KFC')
+		
+end
